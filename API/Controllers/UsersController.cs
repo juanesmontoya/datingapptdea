7f078aad529e2accc6e991f606beb2214e6a9e1c
Juan Esteban Montoya <juanesmontoya92@gmail.com>
2021-04-05 06:22:24 -0500

    version 1

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return users;
+        }
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<AppUser> Users {get; set;}
+
+
+    }
+}
